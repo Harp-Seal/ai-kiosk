7d2a250fa423bf4adb02bee75a14422927f9c477
Ethan Slepner <56898342+Harp-Seal@users.noreply.github.com>
2020-01-14 15:39:59 -0500

    Update camera.py

--- tests/camera.py
+++ tests/camera.py
@@ -3,3 +3,5 @@
 Will be filled in later once I retrieve the code from the Jetson Nano.
 
 """
+$ ls /dev/video0
+$ gst-launch-1.0 nvarguscamerasrc ! 'video/x-raw(memory:NVMM),width=3820, height=2464, framerate=21/1, format=NV12' ! nvvidconv flip-method=0 ! 'video/x-raw,width=960, height=616' ! nvvidconv ! nvegltransform ! nveglglessink -e
